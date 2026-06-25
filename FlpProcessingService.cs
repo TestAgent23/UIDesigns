2000be04a2fac53dbd5e8f1d51c760262e8abe0f
Tiera johnson <92841507+AngularUIFeatures@users.noreply.github.com>
2026-06-25 15:31:35 +0530

    Update FlpProcessingService.cs

--- FlpProcessingService.cs
+++ FlpProcessingService.cs
@@ -1,10 +1,13 @@
- flpProcessTempFile.Name = destinationBlobClient.Name;
-                    flpProcessTempFile.Uri = destinationBlobClient.Uri.ToString();
-                    flpProcessTempFile.CanGenerateSasUri = destinationBlobClient.CanGenerateSasUri;
+  public static bool IsSpreadsheetFile(string fileName) =>
+        Path.GetExtension(fileName).ToLowerInvariant() is ".xlsx" or ".xls" or ".xlsb" or ".xlsm"
+            or ".xltx" or ".xltm" or ".csv" or ".tsv" or ".ods";
+}
 
 
 
 
-
-  flpProcessTempFile.sourceTempFilePath = tempFilePath;
-                   
+return values.EnumerateArray()
+            .Select(e => CommonUtilities.MapSharePointItem(e, folderPath))
+            .Where(i => i.IsFolder || CommonUtilities.IsSpreadsheetFile(i.Name))
+            .ToList();
+    }
