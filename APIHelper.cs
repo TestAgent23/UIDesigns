2fff3cfb647feb453e4ef19357fd899111dd7ca0
Tiera johnson <92841507+AngularUIFeatures@users.noreply.github.com>
2026-07-01 21:48:41 +0530

    Update APIHelper.cs

--- APIHelper.cs
+++ APIHelper.cs
@@ -37,6 +37,15 @@ private static string BuildSharePointFilePath(string? path, string? webUrl, stri
                     var isValidFile2 = await _fileLoadingProcessConfiguration.IsValidFile(flpConfigurationRequestDto.FlpConfigurationId, sharePointFileName, ".xls");
                     var isValidFile3 = await _fileLoadingProcessConfiguration.IsValidFile(flpConfigurationRequestDto.FlpConfigurationId, sharePointFileName, ".xlsb");
                     if (!isValidFile1 && !isValidFile2 && !isValidFile3)
+
+                        var sharePointFileName = !string.IsNullOrWhiteSpace(flpRequestDto.SharePointFileLocation.FileName)
+                        ? flpRequestDto.SharePointFileLocation.FileName
+                        : flpRequestDto.SharePointFileLocation.FileUrl;
+                    var isValidFile1 = await _fileLoadingProcessConfiguration.IsValidFile(flpConfigurationRequestDto.FlpConfigurationId, sharePointFileName, ".xlsx");
+                    var isValidFile2 = await _fileLoadingProcessConfiguration.IsValidFile(flpConfigurationRequestDto.FlpConfigurationId, sharePointFileName, ".xls");
+                    var isValidFile3 = await _fileLoadingProcessConfiguration.IsValidFile(flpConfigurationRequestDto.FlpConfigurationId, sharePointFileName, ".xlsb");
+                    if (!isValidFile1 && !isValidFile2 && !isValidFile3)
+                    
                     
 
 
